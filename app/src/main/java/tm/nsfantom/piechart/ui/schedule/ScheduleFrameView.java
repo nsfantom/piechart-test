9c3eeea9ac9e63423ac5ce2f3205b04d9b5bf540
nsfantom <nick@tatarenkov.name>
2018-12-05 11:40:53 +0200

    add checkbox array

--- app/src/main/java/tm/nsfantom/piechart/ui/schedule/ScheduleFrameView.java
+++ app/src/main/java/tm/nsfantom/piechart/ui/schedule/ScheduleFrameView.java
@@ -1,6 +1,8 @@
 package tm.nsfantom.piechart.ui.schedule;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatCheckBox;
@@ -19,14 +21,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import tm.nsfantom.piechart.R;
+import tm.nsfantom.piechart.util.DisplayUtil;
 
 public class ScheduleFrameView extends FrameLayout {
     private View layout;
     private TableLayout tableLayout;
 
-    private ScheduleModel model;
     private Map<String, SparseBooleanArray> days = new HashMap<>();
-    private TableRow rowHeader;
 
     public ScheduleFrameView(@NonNull Context context) {
         super(context);
@@ -50,12 +51,18 @@ public class ScheduleFrameView extends FrameLayout {
     }
 
     public void setModel(ScheduleModel model) {
-        this.model = model;
         tableLayout.removeAllViews();
 
-        rowHeader = new TableRow(getContext());
+        Resources resources = getResources();
+        TableRow rowHeader = new TableRow(getContext());
         TextView tvTitle = new TextView(getContext());
         tvTitle.setText(model.getTitle());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            tvTitle.setTextAppearance(R.style.ScheduleDaysHoliday);
+        }
+        // TODO: 12/5/18 set font style for old API
+        tvTitle.setGravity(Gravity.CENTER_VERTICAL);
+        tvTitle.setHeight(DisplayUtil.dpToPx(resources, 35));
         rowHeader.addView(tvTitle);
 
         int rowCount = model.getPeriods().length;
@@ -65,33 +72,60 @@ public class ScheduleFrameView extends FrameLayout {
         TableRow tempRow;
         TextView tempTV;
         AppCompatCheckBox checkBox;
-
+        TableRow.LayoutParams cbLayoutParams;
+        int margin = DisplayUtil.dpToPx(resources, 1);
+        String text;
         for (int j = 0; j < columnCount; j++) {
             tempTV = new TextView(getContext());
-            tempTV.setText(model.getDays()[j]);
+            text = model.getDays()[j];
+            tempTV.setText(text);
             tempTV.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            tempTV.setGravity(Gravity.CENTER);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (text.toLowerCase().equals("s"))
+                    tempTV.setTextAppearance(R.style.ScheduleDaysHoliday);
+                else tempTV.setTextAppearance(R.style.ScheduleDays);
+            }
+            // TODO: 12/5/18 set font style for old API
             rowHeader.addView(tempTV);
         }
         tableLayout.addView(rowHeader);
-        tableLayout.setStretchAllColumns(true);
+        tableLayout.setColumnStretchable(0, true);
+        tableLayout.setColumnStretchable(1, false);
         for (int i = 0; i < rowCount; i++) {
             tempRow = new TableRow(getContext());
             tempTV = new TextView(getContext());
             String periodName = model.getPeriods()[i];
             tempTV.setText(periodName);
-            tempRow.addView(tempTV);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                tempTV.setTextAppearance(R.style.ScheduleDaysPeriod);
+            }
+            // TODO: 12/5/18 set font style for old API
+            tempTV.setBackground(resources.getDrawable(R.drawable.schedule_background));
+            tempTV.setGravity(Gravity.START | Gravity.CENTER_VERTICAL);
+            tempTV.setPadding(DisplayUtil.dpToPx(resources, 10), 0, 0, 0);
+
+            tempRow.addView(tempTV, new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.MATCH_PARENT));
             days.put(periodName, new SparseBooleanArray());
             for (int j = 0; j < columnCount; j++) {
                 checkBox = new AppCompatCheckBox(getContext());
+
                 checkBox.setGravity(Gravity.CENTER);
+                checkBox.setButtonDrawable(null);
+                checkBox.setBackground(resources.getDrawable(R.drawable.checkbox_back));
+
 
                 final int index = j;
 
                 checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                    days.get(periodName).put(index,isChecked);
-                    Toast.makeText(getContext(), "clicked: "+periodName+" item: "+index, Toast.LENGTH_SHORT).show();
+                    days.get(periodName).put(index, isChecked);
+                    Toast.makeText(getContext(), "clicked: " + periodName + " item: " + index, Toast.LENGTH_SHORT).show();
                 });
-                tempRow.addView(checkBox, new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
+                tempRow.addView(checkBox);
+                cbLayoutParams = (TableRow.LayoutParams) checkBox.getLayoutParams();
+                cbLayoutParams.setMargins(0, 0, margin, margin);
+                checkBox.setLayoutParams(cbLayoutParams);
+
             }
             tableLayout.addView(tempRow);
         }
